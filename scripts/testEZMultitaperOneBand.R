60747860d673b3f230a7012e85cfda6829694613
CecileLes <139265906+CecileLes@users.noreply.github.com>
2025-12-09 14:59:03 -0600

    Fix visualization until power heatmap
    -

--- scripts/testEZMultitaperOneBand.R
+++ scripts/testEZMultitaperOneBand.R
@@ -1,24 +1,44 @@
+library(Epoch)
+library(ggplot2)
+library(ggtext)
+library(gsignal)
+
+# access package Data in OSF
+dl<-EpochDownloader(progress = FALSE)
+names(dl)
+
 # test EZMultitaper with Epoch package
 # data download one patient one seizure
-dl <- EpochDownloader()
-pt01sz1<-dl$Retrostudy_subpt01_1
+pt01sz1 <- dl$FragilityData_subpt01_1
+pt01sz1
 
 #pt01sz1ieeg<-tblData(pt01sz1)
 
 # crop
-pt01sz1m30p20s<-crop(pt01sz1, start=-30, end=20)
-sozIndex<-which(pt01sz1m30p20s@rowData$soz==TRUE)
+pt01sz1Cropped<-crop(pt01sz1, start=-30, end=20)
 
-display <- c(sozIndex, 77:80)
-# subset on 14 electrodes
-pt01sz1m30p20s14e<-pt01sz1m30p20s[display,]
-sozIndex14e<-which(pt01sz1m30p20s14e@rowData$soz==TRUE)
+# Change gain
+visualSOZ <- function(epoch, sozNames) {
+  p <- plot(epoch)
 
-# in this package visuiEEG uses the epoch package
-visuIEEGData(epoch=pt01sz1m30p20s14e)
+  elecColor <- rep("black", nrow(epoch))
+  elecColor[rownames(epoch) %in% sozNames] <- 'red'
+  elecColor <- rev(elecColor) # match the electrode order in the plot
 
-# Set spectrogram parameters
-# define frequency bands
+  p +
+    geom_vline(xintercept = 0, color = "black", linetype = "dashed", linewidth = 1)+
+    theme(axis.text.y = element_markdown(colour = elecColor))
+}
+
+#To visualize patient 01 seizure 1:
+pt01sozName <- rownames(pt01sz1Cropped)[rowData(pt01sz1Cropped)$soz]
+pt01Display <- c(pt01sozName, "MLT1", "MLT2", "MLT3", "MLT4")
+pt01sz1Reordered <- pt01sz1Cropped[pt01Display, ]
+
+visualSOZ(pt01sz1Reordered, pt01sozName)
+
+#Set spectrogram parameters
+#define frequency bands
 deltaBand<-c(0.5,4)
 thetaBand<-c(4,8)
 alphaBand<-c(8,13)
@@ -30,29 +50,27 @@ rangeBand<-betaBand
 powTimeWindow<-c(0,20)
 baseTimeWindow<-c(-30,-20)
 
-fs=1000
+meta<-metaData(pt01sz1Reordered)
+fs<-metaData(pt01sz1Reordered)$samplingRate
 windowParams<-c(0.25,0.1)
 
 
 # compute the mean power analysis over the frequency band (rangeBand) over time window (powTimeWindow) and baselined time window (baseTimeWindow)
-betaBandPow<-meanPowBaselineBand( epoch=pt01sz1m30p20s14e, fs=fs, windowParams=windowParams, rangeBand=betaBand, powTimeWindow=powTimeWindow, baseTimeWindow=baseTimeWindow)
-
-
-
+betaBandPow<-meanPowBaselineBand( epoch=pt01sz1Reordered, fs=fs, windowParams=windowParams, rangeBand=betaBand, powTimeWindow=powTimeWindow, baseTimeWindow=baseTimeWindow)
 
 ## plot the mean power heatmap
-plotPowBand<-plotPowHeatmap(pow = betaBandPow, sozIndex = sozIndex14e)
+plotPowBand<-plotPowHeatmap(pow = betaBandPow)
 plotPowBand<-plotPowBand+ggplot2::ggtitle(("Mean beta power heatmap for patient pt01"))
 plotPowBand
 
-## plot the mean power quantiles
-plotbetaQuantile<-plotPowQuantile(pow = betaBandPow, sozIndex = sozIndex14e)
-plotbetaQuantile<-plotbetaQuantile+ggplot2::ggtitle(("Pooled mean beta power quantiles for patient pt01"))
-plotbetaQuantile
-
-## plot the mean power distribution
-plotbetaDistr<-plotPowDistribution(pow = betaBandPow, sozIndex = sozIndex14e)
-plotbetaDistr<-plotbetaDistr+ggplot2::ggtitle(("Pooled mean beta power distribution for patient pt01"))
-plotbetaDistr
-
-pt01PowStat <- powStat(pow = betaBandPow, sozIndex = sozIndex14e)
+# ## plot the mean power quantiles
+# plotbetaQuantile<-plotPowQuantile(pow = betaBandPow)
+# plotbetaQuantile<-plotbetaQuantile+ggplot2::ggtitle(("Pooled mean beta power quantiles for patient pt01"))
+# plotbetaQuantile
+#
+# ## plot the mean power distribution
+# plotbetaDistr<-plotPowDistribution(pow = betaBandPow, sozIndex = sozIndex14e)
+# plotbetaDistr<-plotbetaDistr+ggplot2::ggtitle(("Pooled mean beta power distribution for patient pt01"))
+# plotbetaDistr
+#
+# pt01PowStat <- powStat(pow = betaBandPow, sozIndex = sozIndex14e)
